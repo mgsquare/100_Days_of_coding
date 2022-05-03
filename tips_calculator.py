d470f26d3cbcffcdc07747f29ca51cf02e13975a
MGsquare <42298679+mgsquare@users.noreply.github.com>
2022-05-03 21:20:24 +0530

    Update tips_calculator.py

--- tips_calculator.py
+++ tips_calculator.py
@@ -1,6 +1,11 @@
+#$the following program takes the total bill,tip and number of parties as the input to calculate each persons share after tips!
+
+
+
+
 print("Welcome to the tip calculator!\n")
 total = float(input("What was the total bill? "))
 tip = (int(input("How much tip would you like to give? 10, 12 or 15? "))/100) +1
 number_of_people = int(input("How many people to split the bill? "))
 share = float(total*tip/number_of_people)
-print(f"Each person should pay: {round(share,2)}")
+print("Each person should pay: {:.2f}".format(share))
